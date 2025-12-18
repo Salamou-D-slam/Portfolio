3ff876af661c00a2ce809b4813a5281d2691aff0
Islam <islamderrouiche@gmail.com>
2025-12-18 13:07:43 +0100

    http only :true

--- back/main.py
+++ back/main.py
@@ -14,6 +14,7 @@ app = FastAPI()
 app.add_middleware(
     SessionMiddleware, 
     secret_key=os.getenv("SECRET_KEY"),
+    https_only=False,       # cookie envoyé seulement en HTTPS
     same_site="lax",        # protection CSRF
     max_age=60 * 60,        # 1 heure
     )
